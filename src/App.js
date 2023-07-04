052b7d60d94c92f162436a0bf1fffc57d225490b
Mukesh <subramonimukesh@gmail.com>
2023-07-04 18:06:36 +0530

    update app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { AppBar } from "./Components/AppBar";
 import Filters from "./Components/Filters";
 import Footer from "./Components/Footer";
 import Items from "./Components/Items";
+import { SearchBar } from "./Components/SearchBar";
 import SeasonSpecial from "./Components/SeasonSpecial";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Items />
       <SeasonSpecial/>
       <Footer/>
+      <SearchBar/>
     </>
   );
 }
